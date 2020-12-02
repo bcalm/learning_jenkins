7e12690df235a2af0dc008fbfc0a11a089cc7c8a
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-12-02 11:37:38 +0530

    Added test

--- src/main/java/com/learning/jenkins/JenkinsApplication.java
+++ src/main/java/com/learning/jenkins/JenkinsApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsApplication {
     public static void main(String[] args) {
-        String username = System.getProperty("Username");
-        System.out.println(username);
         System.out.println(System.getenv("Username"));
         System.out.println(System.getenv());
     }
--- src/test/java/com/learning/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/learning/jenkins/JenkinsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.learning.jenkins;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JenkinsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public  void shouldGetTheUsername(){
+		Assertions.assertEquals("Vikram", System.getenv("Username"));
+		System.out.println(System.getenv());
 	}
 
 }
